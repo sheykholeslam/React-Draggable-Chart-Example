cda9bad67222407cf4f529cd2b04068612c0ea1b
sheykholeslam <mhs137@mun.ca>
2020-04-09 17:28:05 -0230

    Update DataSet Event
    A component added.

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import React from "react";
 import "./styles.css";
 import DraggableGraph from "./DraggableData";
+import CrazyLine from "./CrazyLine";
 
 export default () => {
   return (
@@ -8,6 +9,7 @@ export default () => {
       <h1>My Draggable Chartjs Demo</h1>
       <h2>By: Mohammad</h2>
       <DraggableGraph />
+      <CrazyLine />
     </div>
   );
 };
--- src/DraggableData.jsx
+++ src/DraggableData.jsx
@@ -2,64 +2,73 @@ import React from "react";
 import { Line } from "react-chartjs-2";
 import "chartjs-plugin-dragdata";
 
-const options = {
-  tooltips: { enabled: true },
-  scales: {
-    xAxes: [
-      {
-        gridLines: { display: false, color: "grey" },
-        ticks: { fontColor: "#3C3C3C", fontSize: 14 }
-      }
-    ],
-    yAxes: [
-      {
-        scaleLabel: {
-          display: true,
-          labelString: "Color Strength",
-          fontSize: 14
-        },
-        ticks: {
-          display: true,
-          min: -5,
-          max: 100,
-          scaleSteps: 50,
-          scaleStartValue: -50,
-          maxTicksLimit: 4,
-          fontColor: "#9B9B9B",
-          padding: 30,
-          callback: point => (point < 0 ? "" : point)
-        },
-        gridLines: {
-          display: false,
-          offsetGridLines: true,
-          color: "3C3C3C",
-          tickMarkLength: 4
-        }
-      }
-    ]
-  },
-  legend: {
-    display: true
-  },
-  dragData: true,
-  onDragStart: function(e) {
-    console.log(e);
-  },
-  onDrag: function(e, datasetIndex, index, value) {
-    console.log(datasetIndex, index, value);
-  },
-  onDragEnd: function(e, datasetIndex, index, value) {
-    console.log(datasetIndex, index, value);
-  }
-};
-
 class DraggableGraph extends React.Component {
   state = {
     dataSet: [[65, 59, 80, 81, 56, 55, 40], [35, 39, 50, 91, 46, 85, 10]],
-    labels: ["January", "February", "March", "April", "May", "June", "July"]
+    labels: ["January", "February", "March", "April", "May", "June", "July"],
+    options: {
+      tooltips: { enabled: true },
+      scales: {
+        xAxes: [
+          {
+            gridLines: { display: false, color: "grey" },
+            ticks: { fontColor: "#3C3C3C", fontSize: 14 }
+          }
+        ],
+        yAxes: [
+          {
+            scaleLabel: {
+              display: true,
+              labelString: "Color Strength",
+              fontSize: 14
+            },
+            ticks: {
+              display: true,
+              min: -5,
+              max: 100,
+              scaleSteps: 50,
+              scaleStartValue: -50,
+              maxTicksLimit: 4,
+              fontColor: "#9B9B9B",
+              padding: 30,
+              callback: point => (point < 0 ? "" : point)
+            },
+            gridLines: {
+              display: false,
+              offsetGridLines: true,
+              color: "3C3C3C",
+              tickMarkLength: 4
+            }
+          }
+        ]
+      },
+      legend: {
+        display: true
+      },
+      dragData: true,
+      dragOptions: {
+        showTooltip: true
+      },
+      dragDataRound: 1,
+      onDragStart: function(e) {
+        console.log("Start:", e);
+      },
+      onDrag: function(e, datasetIndex, index, value) {
+        console.log("Drag:", datasetIndex, index, value);
+      },
+      onDragEnd: function(e, datasetIndex, index, value) {
+        console.log("End:", datasetIndex, index, value, this.state);
+        const set = this.state.dataSet;
+        set[datasetIndex][index] += 5;
+        this.setState({
+          dataSet: set
+        });
+      }.bind(this)
+    }
   };
 
   render() {
+    console.log("RENDER");
     const data = {
       labels: this.state.labels,
       datasets: [
@@ -91,7 +100,7 @@ class DraggableGraph extends React.Component {
 
     return (
       <div>
-        <Line data={data} options={options} />
+        <Line data={data} options={this.state.options} />
       </div>
     );
   }
